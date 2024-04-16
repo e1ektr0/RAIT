b1ae6d567bbee82bf587d75444532cb5c5ab78e9
i.sukhanov <ivan.sukhanov@exilion.com>
2024-04-16 13:28:05 +0300

    support derived type response

--- RAIT.Core/RaitHttpWrapper.cs
+++ RAIT.Core/RaitHttpWrapper.cs
@@ -39,6 +39,20 @@ public class RaitHttpWrapper<TController> where TController : ControllerBase
             prepareInputParameters);
     }
     
+    public async Task<TOutput> CallR<TOutput, TOut>(Expression<Func<TController, Task<TOut>>> tree) where TOutput : TOut
+    {
+        var methodBody = tree.Body as MethodCallExpression;
+        var methodInfo = methodBody!.Method;
+
+        var prepareInputParameters = RaitParameterExtractor.PrepareInputParameters(tree);
+        var rout = RaitRouter.PrepareRout(tree, prepareInputParameters);
+        var output = await RaitHttpRequester.HttpRequest<TOutput?>(_client, methodInfo.CustomAttributes, rout,
+            prepareInputParameters);
+        if (output == null)
+            throw new ArgumentNullException();
+        return output;
+    }
+    
     public async Task<TOutput> CallR<TOutput>(Expression<Func<TController, Task<TOutput>>> tree)
     {
         var methodBody = tree.Body as MethodCallExpression;
@@ -52,6 +66,7 @@ public class RaitHttpWrapper<TController> where TController : ControllerBase
             throw new ArgumentNullException();
         return output;
     }
+    
     public async Task Call(Expression<Func<TController, Task>> tree)
     {
         var methodBody = tree.Body as MethodCallExpression;
