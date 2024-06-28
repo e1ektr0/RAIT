0cea2cd795d24dc2813dcba89eccfb04ce392ad1
Maxim Feofilov <Maxim.Feofilov@gmail.com>
2024-06-28 13:47:58 +0300

    Add include rait extension

--- RAIT.Core/RaitExtensions.cs
+++ RAIT.Core/RaitExtensions.cs
@@ -1,12 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace RAIT.Core;
 
 public static class RaitExtensions
 {
-    public static  RaitHttpWrapper<TController> Rait<TController>(this HttpClient client) where TController : ControllerBase
+    public static RaitHttpWrapper<TController> Rait<TController>(this HttpClient client)
+        where TController : ControllerBase
     {
         return new RaitHttpWrapper<TController>(client);
     }
-}
 
+    public static void IncludeRaitXml(this SwaggerGenOptions swaggerGenOptions)
+    {
+        var executingAssembly = Assembly.GetEntryAssembly();
+
+        var assemblies = executingAssembly!.GetReferencedAssemblies().ToList();
+        assemblies.Add(executingAssembly.GetName());
+        var files = assemblies
+            .Where(assembly => assembly.Name != null)
+            .Select(assembly =>
+                Path.Combine(AppContext.BaseDirectory, $"RAIT\\{assembly.Name}_rait.xml")).ToList();
+        var referencedProjectsXmlDocPaths = files
+            .Where(File.Exists).ToList();
+        foreach (var xmlDocPath in referencedProjectsXmlDocPaths)
+        {
+            swaggerGenOptions.IncludeXmlComments(xmlDocPath);
+        }
+    }
+}
--- RAIT.Example.API/Program.cs
+++ RAIT.Example.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.OpenApi.Models;
+using RAIT.Core;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,8 +8,29 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(swaggerGenOptions =>
+{
+    swaggerGenOptions.SwaggerDoc("v1", new OpenApiInfo { Title = "RAIT.Api", Version = "v1" });
+    var securitySchema = new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        Reference = new OpenApiReference
+        {
+            Type = ReferenceType.SecurityScheme,
+            Id = "Bearer"
+        }
+    };
+    swaggerGenOptions.AddSecurityDefinition("Bearer", securitySchema);
 
+    var securityRequirement = new OpenApiSecurityRequirement { { securitySchema, new[] { "Bearer" } } };
+    swaggerGenOptions.AddSecurityRequirement(securityRequirement);
+            
+    swaggerGenOptions.IncludeRaitXml();
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
