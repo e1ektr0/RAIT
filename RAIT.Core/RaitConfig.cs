26fdc38d33d00ac46718b7f62537bd9467d49f67
Maxim Feofilov <Maxim.Feofilov@gmail.com>
2024-08-08 11:43:18 +0300

    Add UseNewtonsoft support

--- RAIT.Core/RaitConfig.cs
+++ RAIT.Core/RaitConfig.cs
@@ -1,5 +1,7 @@
 ﻿using System.Net.Http.Json;
+using System.Text;
 using System.Text.Json;
+using Newtonsoft.Json;
 using RAIT.Core.DocumentationGenerator.XmlDoc;
 
 namespace RAIT.Core;
@@ -12,7 +14,26 @@ public class RaitConfig
 
     public static bool DocGeneration = true;
 
-    public static Func<HttpContent, Type,Task<object?>>? DeserializeFunction { get; set; }
+    public static void UseNewtonsoft()
+    {
+        SerializeFunction = Serialize;
+        DeserializeFunction = DeserializeAsync;
+    }
+
+    private static async Task<object?> DeserializeAsync(HttpContent content, Type type)
+    {
+        var jsonStr = await content.ReadAsStringAsync();
+        var a = JsonConvert.DeserializeObject(jsonStr, type);
+        return a;
+    }
+
+    private static HttpContent Serialize(object? obj)
+    {
+        var jsonStr = JsonConvert.SerializeObject(obj);
+        return new StringContent(jsonStr, new UTF8Encoding(), "text/json");
+    }
+
+    public static Func<HttpContent, Type, Task<object?>>? DeserializeFunction { get; set; }
 
     public static Func<object?, HttpContent>? SerializeFunction { get; set; }
 }
